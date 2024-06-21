4abb42061ff8fa7654dd0198172165ad6037247e
Felipe Schiavini <52425298+FelipeSchiavini@users.noreply.github.com>
2024-06-21 17:12:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ server.get("/", (req, res) => {
 
   fs.readdir(imagesPath, (err, files) => {
     if (err) {
-      return res.status(500).send('Erro ao ler a pasta de imagens');
+      return res.status(500).send('An error occurred while reading the image.');
     }
 
     const randomImage = files[Math.floor(Math.random() * files.length)];
 
-    const imagePath = path.join(__dirname, imagesPath, randomImage); // Use __dirname here
+    const imagePath = path.join(__dirname, imagesPath, randomImage);
 
     res.sendFile(imagePath);
   });
